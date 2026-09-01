909a6708861fa94fcd851b0688438acc407633d3
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-09-01 17:18:52 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { auth, onAuthStateChanged } from './firebase/firebase';
+import { auth, onAuthStateChanged } from './firebase';
 
 // Components
 import Navbar from './components/Navbar';
