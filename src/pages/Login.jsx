e1143af57b512237cca166fde241e6ea22c9bd03
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-09-01 17:49:21 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { auth, signInWithEmailAndPassword, onAuthStateChanged } from '../firebase/firebase';
+import { auth, signInWithEmailAndPassword, onAuthStateChanged } from '../firebase';
 import './Login.css';
 
 const Login = () => {
